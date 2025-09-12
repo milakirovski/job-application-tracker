0ebb435f54b0e0519e3074e66aef081c5a810788
Mila Kirovski <kirovski.mila@gmail.com>
2025-09-12 17:35:48 +0200

    initial commit

---
+++ jobApplicationTrackerBackend/src/main/java/mk/ukim/finki/jobapplicationtracker/jobapplicationtrackerbackend/JobApplicationTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.jobapplicationtracker.jobapplicationtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobApplicationTrackerBackendApplication.class, args);
+    }
+
+}
---
+++ jobApplicationTrackerBackend/src/test/java/mk/ukim/finki/jobapplicationtracker/jobapplicationtrackerbackend/JobApplicationTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.jobapplicationtracker.jobapplicationtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
