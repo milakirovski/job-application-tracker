fc0b227d2d51173735fef81235f4c101f0b7c8c3
Mila Kirovski <kirovski.mila@gmail.com>
2025-09-18 11:06:31 +0200

    added authentication for the database using env variables

--- jobApplicationTrackerBackend/src/main/java/mk/ukim/finki/jobapplicationtracker/jobapplicationtrackerbackend/JobApplicationTrackerBackendApplication.java
+++ jobApplicationTrackerBackend/src/main/java/mk/ukim/finki/jobapplicationtracker/jobapplicationtrackerbackend/JobApplicationTrackerBackendApplication.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.jobapplicationtracker.jobapplicationtrackerbackend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JobApplicationTrackerBackendApplication {
 
     public static void main(String[] args) {
+
+        // Load environment variables from .env file
+        Dotenv.load();
         SpringApplication.run(JobApplicationTrackerBackendApplication.class, args);
     }
 
