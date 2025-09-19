4cec638119cf9209f9975ea2e12326b83156d4fc
Mila Kirovski <kirovski.mila@gmail.com>
2025-09-19 17:08:38 +0200

    add more generic solution to the base URL

--- jobApplicationTrackerFrontend/src/axios/axios.js
+++ jobApplicationTrackerFrontend/src/axios/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
+// In Vite, environment variables are accessed via `import.meta.env`
+// and must be prefixed with `VITE_` to be exposed to the client.
+// This code now correctly reads the variable and defaults to a relative path.
+
+/*For Kubernetes: When your DockerfileFrontend runs npm run build, it's a production build.
+ Vite does not load the .env.development file. In this case, import.meta.env.VITE_API_URL will be undefined.
+*/
+const baseURL = import.meta.env.VITE_API_URL || "/api";
+
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: baseURL,
     headers: {
         "Content-Type": "application/json",
     },
