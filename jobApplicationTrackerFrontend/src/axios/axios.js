711c6c23d4af545fb2d8e09be447129bd78108ad
Mila Kirovski <kirovski.mila@gmail.com>
2026-01-27 00:18:23 +0100

    update main.yaml, update baseURL in axios.js and docker-compose.yml

--- jobApplicationTrackerFrontend/src/axios/axios.js
+++ jobApplicationTrackerFrontend/src/axios/axios.js
@@ -9,7 +9,7 @@ import axios from "axios";
 */
 
 // const baseURL = import.meta.env.VITE_API_URL;
-const baseURL = "/api";
+const baseURL = "http://localhost:8080/api";
 
 const axiosInstance = axios.create({
     baseURL: baseURL,
