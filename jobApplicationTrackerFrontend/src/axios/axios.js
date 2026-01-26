e3bb6a4306fa1e12659734a92e14641ca78cdf47
Mila Kirovski <kirovski.mila@gmail.com>
2026-01-27 00:43:34 +0100

    remove again unnecessary comments in axios.js

--- jobApplicationTrackerFrontend/src/axios/axios.js
+++ jobApplicationTrackerFrontend/src/axios/axios.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-// In Vite, environment variables are accessed via `import.meta.env`
-// and must be prefixed with `VITE_` to be exposed to the client.
-// This code now correctly reads the variable and defaults to a relative path.
-
-/*For Kubernetes: When your DockerfileFrontend runs npm run build, it's a production build.
- Vite does not load the .env.production file. In this case, import.meta.env.VITE_API_URL will be undefined.
-*/
 
 const baseURL = "http://localhost:8080/api";
 
